d5462d50283de1f8fc25bcf231a47270e92a6196
Basem Walid <basemwalid44@gmail.com>
2025-03-23 00:20:54 +0200

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,84 @@
+let play_btn = document.getElementById('play');
+let video = document.getElementById('video');
+
+
+play_btn.addEventListener('click', () => {
+    if (video.paused) {
+        video.play();
+        video.style.display = 'unset';
+        play_btn.classList.remove('bi-play-fill');
+        play_btn.classList.add('bi-pause');
+    } else {
+        video.pause();
+        video.style.display = 'none';
+        play_btn.classList.add('bi-play-fill');
+        play_btn.classList.remove('bi-pause');
+    }
+})
+
+video.addEventListener('ended', () => {
+    video.play();
+})
+
+
+
+const container = document.querySelector(".right .seats .container");
+const seats = document.querySelectorAll(".right .seats .row .seat:not(.sold)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+populateUI();
+
+
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".right .seats .row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats1", JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * 200;
+  localStorage.setItem("countm",selectedSeatsCount);
+  
+}
+
+
+// Get data from localstorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats1"));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        console.log(seat.classList.add("selected"));
+      }
+    });
+  }
+
+
+}
+console.log(populateUI())
+
+// Seat click event
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("sold")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
+
+function receiptpage() {
+
+  window.location = "Menu.html";
+}
+
+// Initial count and total set
+updateSelectedCount();
